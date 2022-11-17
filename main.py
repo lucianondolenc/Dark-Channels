537cabc1f01937c99e0417409425d5dd2d4d092e
Luciano Dolenc <lucianondolenc@gmail.com>
2022-11-16 22:15:15 -0300

    1611

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import json
 from downloader.downloader_handler import DownloaderHandler
 from renderer.rederer_handler import RendererHandler
+from utils import Utils
 
 
 def main():
     channels = json.load(open('channels.json'))
     for channel in channels:
-        response = DownloaderHandler(channel=channel['nome_canal']).handler()
-        response = RendererHandler(channel=channel['nome_canal'], series_folder=response['series_folder']).handler()
+        response_downloader = DownloaderHandler(channel=channel['nome_canal']).handler()
+        response_renderer = RendererHandler(channel=channel['nome_canal'], series_folder=response_downloader['series_folder']).handler()
+
+        for item in response_downloader['information']:
+
+            Utils(item_name=item['item_name'], url_list=item['error_list'], file_path=f'channels/{channel["nome_canal"]}/Arquivos/error/error_{item["item_name"]}.txt').update_file()
+            Utils(item_name=item['item_name'], url_list=item['historic_list'], file_path=f'channels/{channel["nome_canal"]}/Arquivos/historic/historic_{item["item_name"]}.txt').update_file()
+            Utils(item_name=item['item_name'], url_list=item['historic_list'], file_path=f'channels/{channel["nome_canal"]}/Arquivos/urls/{item["item_name"]}.txt').update_file()
 
 
 if __name__ == '__main__':
