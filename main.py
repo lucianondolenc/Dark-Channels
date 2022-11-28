facdd13b1c7c6cfbea9373e103b0fb1b90ce9b9d
Luciano Dolenc <lucianondolenc@gmail.com>
2022-11-27 21:50:40 -0300

    2711

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ def main():
 
         for item in response_downloader['information']:
 
+            # Update Files
             Utils(item_name=item['item_name'], url_list=item['error_list'], file_path=f'channels/{channel["nome_canal"]}/Arquivos/error/error_{item["item_name"]}.txt').update_file()
             Utils(item_name=item['item_name'], url_list=item['historic_list'], file_path=f'channels/{channel["nome_canal"]}/Arquivos/historic/historic_{item["item_name"]}.txt').update_file()
             Utils(item_name=item['item_name'], url_list=item['historic_list'], file_path=f'channels/{channel["nome_canal"]}/Arquivos/urls/{item["item_name"]}.txt').update_file()
 
+            # Create title, description and hashtags
+            Utils(item_name=item['item_name'], chanel=channel['nome_canal'],series_folder=response_downloader['series_folder']).generate_title_description_hashtag()
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -75,7 +75,34 @@ class Utils:
         background = random.sample(backgrounds_list, 1)[0]
         return background
 
+    def generate_title_description_hashtag(self):
+        hashtag_combo_1 = self.generate_hashtag_combo()
+        hashtag_combo_2 = self.generate_hashtag_combo()
+
+        title = open(f'channels/{self.channel}/Arquivos/titles/{self.item_name}_title.txt', 'r', encoding='utf-8').readlines()[0]
+        file = open(f'channels/{self.channel}/Arquivos/utils/base_description.txt', 'r', encoding='utf-8')
+        data = file.read()
+        data = data.replace('Título', title).replace('Hashtags_1', hashtag_combo_1).replace('Hashtags_2', hashtag_combo_2)
+        file.close()
+
+        file = open(f'channels/{self.channel}/Videos/{self.series_folder}/Video_{self.item_name}/title_description_hashtag.txt', 'w', encoding='utf-8')
+        file.write(data)
+        file.close()
+        return None
+
+
+    def generate_hashtag_combo(self):
+        file = open(f'channels/{self.channel}/Arquivos/keywords/{self.item_name}_keywords.txt', 'r', encoding='utf-8')
+        lines = file.readlines()
+        lines = random.sample(lines, 45)
+        hashtag_combo = ''
+        for line in lines:
+            line = line.replace('#', '')
+            line = line.split()[0]
+            hashtag_combo = hashtag_combo + line + ','
+        hashtag_combo = hashtag_combo[:-1]
+        return hashtag_combo
 
 
 # if __name__ == '__main__':
-#     Utils(channel='Dopaminando').get_random_background()
+#     Utils(item_name='asmr',channel='Dopaminando', series_folder='Video_Series_11-21-2022_21-54-41').generate_title_description_hashtag()
